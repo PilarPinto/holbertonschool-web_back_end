100cdfa7e7a87a1ec662e130c8b5313bb382c222
PilarPinto <piapintoch@unal.edu.co>
2021-03-16 01:29:41 +0000

    [Feature]:Using an math function with type annotation

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+Using a library to invoque
+a function of floor of one
+float number, is the number most little
+of a number e.g 2.3 = 2
+'''
+import math
+
+
+def floor(n: float) -> int:
+    '''
+    Has a number param that are float
+    return just the int that represents
+    the floor
+    '''
+    return math.floor(n)
