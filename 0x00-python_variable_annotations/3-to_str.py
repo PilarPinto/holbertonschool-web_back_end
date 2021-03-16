a0116b35f80d802331a40157140a6101534d9a3a
PilarPinto <piapintoch@unal.edu.co>
2021-03-16 01:31:41 +0000

    [Feature]: Type annotation with cast

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Convert a number that has a float
+point to an array of chars
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    Using the cast to return and number
+    and using the type annotation
+    '''
+    return str(n)
