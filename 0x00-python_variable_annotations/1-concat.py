19191f31feb0932ecb9e6f9a7e11d27da31e3b68
PilarPinto <piapintoch@unal.edu.co>
2021-03-16 01:25:50 +0000

    [Feature]:Basic annotation concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Basic annotation in a Concat
+definition
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    Concat two annotated string
+    with str type
+    '''
+    return str1 + str2
