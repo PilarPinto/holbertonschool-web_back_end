01f2fd5e3015e1f9879b101c8f411e01cc288772
PilarPinto <piapintoch@unal.edu.co>
2021-03-16 01:36:32 +0000

    [T]

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Define variables with type annotation
+'''
+
+a: int
+a = 1
+pi: float
+pi = 3.14
+i_understand_annotations: bool
+i_understand_annotations = True
+school: str
+school = "Holberton"
