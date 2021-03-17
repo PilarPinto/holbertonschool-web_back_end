dd16012b7a3d54417e4cade62c09fe3a95e592e1
PilarPinto <piapintoch@unal.edu.co>
2021-03-17 01:46:31 +0000

    [Feature]: Using a coroutine and Random

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 '''Using a coruotine'''
-
 import asyncio
 from random import random
 
 
-async def wait_random(max_delay=10):
-    '''Asyn with random'''
+async def wait_random(max_delay: int = 10) -> float:
+    '''Async with random'''
     ran_num = random() * max_delay
     await asyncio.sleep(ran_num)
     return ran_num
