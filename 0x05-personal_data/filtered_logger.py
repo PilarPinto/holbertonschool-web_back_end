a3fc1480b1c2482b57f9c8b354b015e7d93cf990
PilarPinto <piapintoch@unal.edu.co>
2021-03-30 19:57:57 +0000

    [T]

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -3,18 +3,11 @@
 import re
 from typing import List
 
-patterns = {
-    "password": "[a-zA-Z]*",
-    "date_of_birth": r"([0-9]{2}\/){2}[0-9]{4}",
-    "name": "[a-zA-Z]*",
-    "email": r"[a-zA-Z]*@[a-zA-Z]*\.[a-zA-Z]*",
-}
-
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     '''Filter definition using Regex'''
     for f in fields:
-        message = re.sub(f + "=" + patterns[f] + "?" + separator,
+        message = re.sub(f + "=.*?" + separator,
                          f + "=" + redaction + separator, message)
     return message
