8d0965e2fdabbffaaee904a8a15b39929d8a73c9
PilarPinto <piapintoch@unal.edu.co>
2021-03-30 19:55:46 +0000

    [T]

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -15,6 +15,6 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     '''Filter definition using Regex'''
     for f in fields:
-        message = re.sub(f + "=" + patterns[f] + separator + "{1}",
-                         "{}={}{}".format(f, redaction, separator), message)
+        message = re.sub(f + "=" + patterns[f] + "?" + separator,
+                         f + "=" + redaction + separator, message)
     return message
