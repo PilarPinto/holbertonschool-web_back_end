1b7b647293611247fd830520f6ab99682d039049
PilarPinto <piapintoch@unal.edu.co>
2021-03-30 22:50:57 +0000

    [Feature]:Create logger

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -4,6 +4,8 @@ import re
 from typing import List
 import logging
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
@@ -30,3 +32,16 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f + "=.*?" + separator,
                          f + "=" + redaction + separator, message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    '''Definition that takes no arguments
+    and returns a logging.Logger object.'''
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(RedactingFormatter(PII_FIELDS))
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
