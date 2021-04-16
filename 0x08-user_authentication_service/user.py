db806b42236b1fedadb3e2909d087ce1b22cddef
PilarPinto <piapintoch@unal.edu.co>
2021-04-16 19:06:51 +0000

    [Feature]: User model

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""Users model
 """
-import sqlalchemy
+Users model
+"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
