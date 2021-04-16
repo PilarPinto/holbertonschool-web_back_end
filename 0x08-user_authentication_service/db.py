cbf7d035ed99430fc57317f5e5f9df35bd335545
PilarPinto <piapintoch@unal.edu.co>
2021-04-16 21:29:55 +0000

    [Test]

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,57 +1,54 @@
 #!/usr/bin/env python3
-"""DB module
+"""te, find and update user
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-
 from user import Base, User
 
 
 class DB:
-    """DB class
-    """
+    """DB class definition"""
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
+    def __init__(self):
+        """Constructor method"""
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
-        """
+    def _session(self):
+        """Session maker"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """add user in a database
-        """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
+        """Adds and saves a new user to the DB"""
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
         self._session.commit()
-        return user
+        return new_user
 
-    def find_user_by(self, **filters) -> User:
-        """Return the user that matches the input
-        """
-        if not User.__dict__.get(*filters):
+    def find_user_by(self, **kwargs) -> User:
+        """Returns the first row found in the users table as filtered by
+        the method’s input arguments"""
+        if not kwargs:
             raise InvalidRequestError
-        query = self._session.query(User).filter_by(**filters)
-        if not query.first():
+
+        search_user = self._session.query(User).filter_by(**kwargs).first()
+
+        if not search_user:
             raise NoResultFound
-        return query.first()
+
+        return search_user
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """Updates user that passes in the args"""
+        """Updates the user"""
         user_to_update = self.find_user_by(id=user_id)
 
         for key, val in kwargs.items():
