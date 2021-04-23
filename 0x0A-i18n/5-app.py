d9f573300a49eee4099e001155e9fd7153a4d67c
PilarPinto <piapintoch@unal.edu.co>
2021-04-23 01:42:52 +0000

    [Feature]: Template

---
+++ 0x0A-i18n/5-app.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+Flask App Project
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Config app class
+    """
+    LANGUAGES = ["en", "fr"]
+
+
+app.config.from_object(Config)
+Babel.default_locale = "en"
+Babel.default_timezone = "UTC"
+
+users = {
+}
+
+
+def get_user(login_as):
+    """Gets a user from request
+    """
+    if login_as and int(login_as) in users:
+        return users.get(int(login_as))
+    return None
+
+
+@app.before_request
+def before_request():
+    """Excecuted before every request
+    """
+    u_id = request.args.get("login_as")
+    if u_id:
+        user = get_user(u_id)
+        g.user = user
+    else:
+        g.user = None
+
+
+@babel.localeselector
+def get_locale():
+    """Select a language translation to use in every request
+    """
+    locale = request.args.get("locale")
+    if locale:
+        return locale
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route("/", methods=["GET"])
+def welcome():
+    """GET /
+    returns render of 5-index.html"""
+    return render_template("5-index.html", user=g.user)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
