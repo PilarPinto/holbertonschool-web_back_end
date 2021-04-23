ed475d87e5dd6a12f0a28f17651761a6e7489146
PilarPinto <piapintoch@unal.edu.co>
2021-04-23 01:45:06 +0000

    [Feature]:Mock logging

--- 0x0A-i18n/5-app.py
+++ 0x0A-i18n/5-app.py
@@ -20,6 +20,10 @@ Babel.default_locale = "en"
 Babel.default_timezone = "UTC"
 
 users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
 
