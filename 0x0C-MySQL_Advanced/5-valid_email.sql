47e2baf899aa79f2cc82a071ef611d57b7eff152
PilarPinto <piapintoch@unal.edu.co>
2021-04-29 23:41:03 +0000

    [Feature]:Triggers and resets

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+--Creates a trigger that resets
+DELIMITER //
+CREATE TRIGGER resets
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN IF NEW.email <> OLD.email THEN
+SET NEW.valid_email = 0;
+END IF;
+END;//
+DELIMITER ;
