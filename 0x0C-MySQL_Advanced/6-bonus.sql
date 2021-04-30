c4da506d3a8a7b8c5ac14b384715c22eb0a17f9d
PilarPinto <piapintoch@unal.edu.co>
2021-04-30 00:21:07 +0000

    [Feature]:Create procedure

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -1,13 +1,21 @@
 --Creates a stored procedure
-DELIMITER //
-CREATE PROCEDURE AddBonus
-(IN user_id INTEGER, IN project_name VARCHAR(255), IN score INTEGER)
-BEGIN
-    INSERT INTO projects (name)
-    SELECT project_name from DUAL
-    WHERE NOT EXISTS (SELECT * FROM projects WHERE name=project_name LIMIT 1);
+DROP PROCEDURE IF EXISTS AddBonus;
 
-    INSERT INTO corrections (user_id, project_id, score)
-    VALUES(user_id, (SELECT id from projects WHERE name=project_name), score);
-END//
+DELIMITER //
+CREATE PROCEDURE AddBonus( IN user_id INT, IN project_name VARCHAR(255), IN score FLOAT) BEGIN DECLARE project_id INT; IF (
+
+SELECT  COUNT(*)
+FROM projects
+WHERE name = project_name) = 0 THEN 
+
+INSERT INTO projects (name) VALUES (project_name); END IF;
+
+SET project_id = (
+SELECT  id
+FROM projects
+WHERE name = project_name
+LIMIT 1);
+
+INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, project_id, score);
+END //
 DELIMITER ;
