e9d5ef9aca14b151d270a6e8e1ee52833f94070f
PilarPinto <piapintoch@unal.edu.co>
2021-04-30 01:31:27 +0000

    [D]

--- 0x0C-MySQL_Advanced/5-valid_email.sql
+++
@@ -1,8 +0,0 @@
---Creates a trigger that resets
-DELIMITER $$
-CREATE TRIGGER val_email BEFORE
-UPDATE ON users FOR EACH ROW BEGIN IF NEW.email <> OLD.email THEN
-SET NEW.valid_email = 0;
-END IF;
-END $$
-DELIMITER ;
