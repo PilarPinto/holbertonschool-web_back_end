ba0a6a8b49473f31306cdf83f4ec401ed394c498
PilarPinto <piapintoch@unal.edu.co>
2021-05-05 02:23:08 +0000

    [Test]

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
-"""Print logs """
+"""
+Log stats
+"""
+
+
 from pymongo import MongoClient
 
+
 if __name__ == "__main__":
-    """ check for all elements in a collection """
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
-    print(f"{collection.estimated_document_count()} logs")
-
+    number_documents = collection.count_documents({})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print(f"{number_documents} logs")
     print("Methods:")
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        method_count = collection.count_documents({'method': method})
-        print(f"\tmethod {method}: {method_count}")
-
-    check_get = collection.count_documents(
-        {'method': 'GET', 'path': "/status"})
-    print(f"{check_get} status check")
+    for method in methods:
+        n_docs = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {n_docs}")
+    number_docs_path = collection.count_documents({
+        "$and": [
+            {"method": "GET"},
+            {"path": "/status"}
+        ]
+    })
+    print(f"{number_docs_path} status check")
