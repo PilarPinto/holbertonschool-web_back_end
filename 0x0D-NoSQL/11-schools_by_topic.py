4c1d3ad8689e3f4f40562750a581a774bea78c42
PilarPinto <piapintoch@unal.edu.co>
2021-05-05 02:16:21 +0000

    [Test]

--- 0x0D-NoSQL/11-schools_by_topic.py
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Returns the list of school having a specific topic"""
+
+
 def schools_by_topic(mongo_collection, topic):
     """Returns the list of school having specific topic
     """
-    return (mongo_collection.find({"topics": topic}))
+    return [i for i in mongo_collection.find({"topics": topic})]
