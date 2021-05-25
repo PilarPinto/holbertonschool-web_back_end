a86b2072ff325e368f41fb61d4d11261b7543513
PilarPinto <piapintoch@unal.edu.co>
2021-05-25 00:00:27 +0000

    [Test]:Fix style

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -5,15 +5,15 @@ function countStudents(path) {
     fs.readFile(path, 'utf-8', (err, dataDb) => {
       if (err) reject(Error('Cannot load the database'));
       if (dataDb) {
-        const inFields = {}; 
+        const inFields = {}; // {field: {counter: # of students, students: [list of students]}}
         const data = dataDb.split('\n');
         console.log(`Number of students: ${data.length - 1}`);
 
         for (let i = 1; i < data.length; i += 1) {
-          const line = data[i].split(','); 
-          if (inFields[line[3]]) { 
+          const line = data[i].split(','); // get each word
+          if (inFields[line[3]]) { // line[3] is the name field
             inFields[line[3]].counter += 1;
-            inFields[line[3]].students.push(` ${line[0]}`);
+            inFields[line[3]].students.push(` ${line[0]}`); // line[0] is firstname
           } else {
             inFields[line[3]] = { counter: 1, students: [`${line[0]}`] };
           }
@@ -23,6 +23,7 @@ function countStudents(path) {
             console.log(`Number of students in ${key}: ${inFields[key].counter}. List: ${inFields[key].students}`);
           }
         }
+        // ret obj of students in fields and number of students
         resolve({ inFields, counter: data.length - 1 });
       }
     });
